8bff203df6e71f80c3db6833086f0ee7fc74d04c
ibazak <izabela.bazak@gmail.com>
2016-08-11 19:31:53 +0200

    app.js ajax

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
 $(document).foundation();
+
+$(document).ready(function(){
+    $('.next').click(function(){
+       alert($(".login-input").val());
+    });
+
+
+
+    $('.next').click(function() {
+        var loginValue = $(".login-input").text();
+        var passwordValue = $(".password-input").val();
+        var LogPass = {
+            'login': loginValue,
+            'password': passwordValue
+        };
+        sendAjax(LogPass);
+    });
+
+    function sendAjax(pValue) {
+        $.ajax({
+            type: "post",
+            data: pValue,
+            url: "https://efigence-camp.herokuapp.com/api/login",
+            error: function(response) {
+                console.log(response.responseText);
+            },
+            success: function(response) {
+                console.log(response);
+            }
+        });
+    };
+});
+
+
+
