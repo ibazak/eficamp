10be7c8caccfb4d0e9acbe0ece58e4dfb2dd350e
ibazak <izabela.bazak@gmail.com>
2016-08-16 22:48:58 +0200

    login buttons backgrund

--- js/app.js
+++ js/app.js
@@ -10,7 +10,6 @@ $(document).ready(function(){
             'password': passwordValue
         };
         sendAjax(LogPass);
-
     });
 
     function sendAjax(pValue) {
@@ -23,28 +22,15 @@ $(document).ready(function(){
                 document.getElementById("alertbox").innerHTML  =
                     "Nieprawidłowy identyfikator lub hasło. <br>" +
                     "Upewnij się, że CAPS LOCK jest wyłączony i wpisz ponownie dane logowania";
+                //.load() .replace() w divie
                 $("#alertbox").delay(3000).fadeOut(1000);
             },
             success: function(response) {
                 console.log(response);
-                if (response.readyState == 4) {
-                    document.getElementById("demo").innerHTML = response.responseText;
-                    $(".go-button").attr("href", "http://www.w3schools.com/jquery");
-                } else {
-                    document.getElementById("demo").innerHTML = "Nieobsłużony wyjątek";
-                }
+                window.location.replace("html/dashboard.html");
             }
         });
     };
-
-    /*function goToDashboard() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (xhttp.readyState == 4 && xhttp.status == 200) {
-                document.getElementById("demo").innerHTML = xhttp.responseText;
-            }
-        };
-    };*/
 
 });
 
