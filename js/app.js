afaf1c7c5525013b40ddd282d3903b8b86799657
ibazak <izabela.bazak@gmail.com>
2016-08-16 22:34:05 +0200

    everything important

--- js/app.js
+++ js/app.js
@@ -1,12 +1,8 @@
 $(document).foundation();
 
 $(document).ready(function(){
-    $('.next').click(function(){
-       alert($(".login-input").val());
-    });
 
-
-    $('.go').click(function() {
+    $('.go-button').click(function() {
         var loginValue = $(".login-input").text();
         var passwordValue = $(".password-input").val();
         var LogPass = {
@@ -14,6 +10,7 @@ $(document).ready(function(){
             'password': passwordValue
         };
         sendAjax(LogPass);
+
     });
 
     function sendAjax(pValue) {
@@ -23,13 +20,31 @@ $(document).ready(function(){
             url: "https://efigence-camp.herokuapp.com/api/login",
             error: function(response) {
                 console.log(response.responseText);
+                document.getElementById("alertbox").innerHTML  =
+                    "Nieprawidłowy identyfikator lub hasło. <br>" +
+                    "Upewnij się, że CAPS LOCK jest wyłączony i wpisz ponownie dane logowania";
+                $("#alertbox").delay(3000).fadeOut(1000);
             },
             success: function(response) {
                 console.log(response);
+                if (response.readyState == 4) {
+                    document.getElementById("demo").innerHTML = response.responseText;
+                    $(".go-button").attr("href", "http://www.w3schools.com/jquery");
+                } else {
+                    document.getElementById("demo").innerHTML = "Nieobsłużony wyjątek";
+                }
             }
         });
     };
 
+    /*function goToDashboard() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                document.getElementById("demo").innerHTML = xhttp.responseText;
+            }
+        };
+    };*/
 
 });
 
